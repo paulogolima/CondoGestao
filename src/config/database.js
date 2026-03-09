45b522991753ce4c6871933b75f86e9cf81e1de7
paulogolima <paulogolimacontato@gmail.com>
2026-03-09 13:04:05 -0300

    Configuração da conexão com o Banco de Dados

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import Sequelize from 'sequelize'
+
+const connection = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host:process.env.DB_HOST,
+        dialect:process.env.DB_DIALECT
+    }
+)
+
+export default connection
