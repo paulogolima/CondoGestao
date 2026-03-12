f77f50924af085856518a44edf0cca9a8f1bab4b
paulogolima <paulogolimacontato@gmail.com>
2026-03-12 15:24:09 -0300

    feat: adicionar model de usuario com criptografia de senha

---
+++ backend/src/model/User.js
@@ -0,0 +1,43 @@
+// Importaçao de dependências e configuração do ambiente
+import {DataTypes} from 'sequelize'
+import bcrypt from 'bcrypt'
+import connection from '../config/database.js'
+
+const User = connection.define('User',{
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        set(value) {
+            this.setDataValue("email", value.toLowerCase())
+        },
+        validate: {
+            isEmail: {
+                mensagem: "O formato do e-mail é inválido"
+            }
+        }
+    },
+    password:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role:{
+        type: DataTypes.ENUM('admin', 'user'),
+        defaultValue: 'user',
+        allowNull: false
+    }
+})
+
+// Ação para criptografar a senha antes de salvar no banco de dados
+User.addHook('beforeSave', async (user) => {
+    if(user.password){
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(user.password, salt)
+    }
+})
+
+export default User
